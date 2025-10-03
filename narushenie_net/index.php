73406abd87db220efd92475d8915eed6f099e9ac
kabinet303 <kabinet303@localhost.localdomain>
2025-10-03 11:02:22 +0300

    fshbdhtzs

---
+++ narushenie_net/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>нарушениям нет</title>
+    <link rel='icon' href='images/logo.jpeg'>
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+<body>
+    <header> <img src='images/logo.jpeg' alt='логотип'>
+        <h1>нарушениям нет</h1>
+    </header>
+
+    <nav>
+        <a href="/demoexz/narushenie_net">Главная</a>
+        <a href="/demoexz/narushenie_net/admin">Админ-панель</a>
+    </nav>
+
+    <main>
+        <h1>нарушениям нет</h1>
+        <footer>
+            2025
+        <footer>
+    </main>
+
+    <script src="js/script.js"></script>
+    
+
+</body>
+</html>
